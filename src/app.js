4ae32c4d7db43e2fc25b86ebf72c6b2baff3052c
Fock Space <a86gj3sp4@gmail.com>
2022-06-08 10:21:43 +0800

    using express to public a html file

--- src/app.js
+++ src/app.js
@@ -1,13 +1,37 @@
+const path = require("path");
 const express = require("express");
 
 const app = express();
+const publicDir = path.join(__dirname, "../public")
+
+app.use(express.static(publicDir));
 
 app.get("", (req, res) => {
-  res.send("Hello express!");
+  res.send("<h1>Weather<h/1>");
 });
 
 app.get("/help", (req, res) => {
-  res.send("Help page");
+  res.send([
+    {
+      name: "Fock",
+      age: 27,
+    },
+    {
+      name: "Space",
+      age: 22,
+    },
+  ]);
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1>About</h1>");
+});
+
+app.get("/weather", (req, res) => {
+  res.send({
+    forcast: "It is raining",
+    location: "Taipei",
+  });
 });
 
 // app.com
