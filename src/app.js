dcaad7cb664f8b5136311a104cd5e13a4a2e33b8
Fock Space <a86gj3sp4@gmail.com>
2022-06-08 14:00:11 +0800

    create another html i file

--- src/app.js
+++ src/app.js
@@ -6,27 +6,6 @@ const publicDir = path.join(__dirname, "../public")
 
 app.use(express.static(publicDir));
 
-app.get("", (req, res) => {
-  res.send("<h1>Weather<h/1>");
-});
-
-app.get("/help", (req, res) => {
-  res.send([
-    {
-      name: "Fock",
-      age: 27,
-    },
-    {
-      name: "Space",
-      age: 22,
-    },
-  ]);
-});
-
-app.get("/about", (req, res) => {
-  res.send("<h1>About</h1>");
-});
-
 app.get("/weather", (req, res) => {
   res.send({
     forcast: "It is raining",
@@ -38,6 +17,6 @@ app.get("/weather", (req, res) => {
 // app.com/help
 // app.com/about
 
-app.listen(3000, () => {
+app.listen(3003, () => {
   console.log("Server is up on port 3000.");
 });
