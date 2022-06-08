0283f86e884e0cb7e645e22fe1e7c33d53a29966
Fock Space <a86gj3sp4@gmail.com>
2022-06-08 16:02:45 +0800

    create index.hbs file

--- src/app.js
+++ src/app.js
@@ -2,9 +2,16 @@ const path = require("path");
 const express = require("express");
 
 const app = express();
-const publicDir = path.join(__dirname, "../public")
+const publicDir = path.join(__dirname, "../public");
 
+app.set("view engine", "hbs");
 app.use(express.static(publicDir));
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather App",
+    name: "Fock Space",
+  });
+});
 
 app.get("/weather", (req, res) => {
   res.send({
@@ -17,6 +24,6 @@ app.get("/weather", (req, res) => {
 // app.com/help
 // app.com/about
 
-app.listen(3003, () => {
+app.listen(3000, () => {
   console.log("Server is up on port 3000.");
 });
