c4e90fa27fc0a7ebbd5b59b27b881e4ad3367d2a
Fock Space <a86gj3sp4@gmail.com>
2022-06-13 20:09:39 +0800

    setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,7 +5,9 @@ const request = require("request");
 const chalk = require("chalk");
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
+
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
 const publicDir = path.join(__dirname, "../public");
@@ -104,6 +106,6 @@ app.get("*", (req, res) => {
 // app.com/help
 // app.com/about
 
-app.listen(3000, () => {
-  console.log("Server is up on port 3000.");
+app.listen(port, () => {
+  console.log("Server is up on port" + port);
 });
